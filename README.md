e828341bf04a1b3810151fcfb2632a6402bedada
Dhanvinsh <72690343+Dhanvinsh99@users.noreply.github.com>
2020-10-28 18:51:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# p5.play-boilerplate
-Boiler plate for p5.play
+#  car crash test by dhanvinsh
+car crash test made in p5play.js
--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
   weight = random(80, 90)
 
   car = createSprite(50, 200, 50, 50);
-  car.velocityX = speed;
+  car.velocityX = speed
   car.debug = true;
 
   wall = createSprite(750, 200, 50, 100);
@@ -16,7 +16,7 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255);
+  background(0, 0, 0);
   if(car.x - wall.x < car.width/2 + wall.width/2){
     car.velocityX = 0;
     var deformation = (0.5* weight * speed * speed)/22509
@@ -25,8 +25,7 @@ function draw() {
       car.shapeColor = color(255, 0, 0);
     }
     if(deformation >= 100 && deformation === 180){
-      car.shapeColor =color(200, 0, 250);
-    }
+      car.shapeColor =color(200, 7, 90)
     if (deformation < 100){
       car.shapeColor = color(0, 0, 255);
     }
